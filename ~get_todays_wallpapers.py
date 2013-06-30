f88790437a9c49408cd9e47502b96c8cbcbe939a
Wells Johnston <wellsjohnston@gmail.com>
2013-06-30 13:27:10 -0400

    Image sources are now modular.  You can also choose the number of images you want downloaded per cron job as well as whether you want downloading new images to overwrite old ones.

--- ~get_todays_wallpapers.py
+++ ~get_todays_wallpapers.py
@@ -2,14 +2,27 @@
 
 import urllib2, re, os, sys, glob
 
+# a list of sites you want to extract pictures from
+# defaults are Reddit's cityporn and earthporn
+image_sources = ['http://www.reddit.com/r/earthporn', 'http://www.reddit.com/r/cityporn']
+
+# the number of pictures you want to be downloaded every time
+image_limit = 30
+
+# destructive downloading means that old wallpaper images are overwritten by new ones
+# if set to False, all the wallpapers will be saved.
+destructive = True
+
+# modify this to extract the right url pattern.  
+# Default is set to http://i.imgur.com/{picture_name}.jpg
 image_regex = re.compile('http://i.imgur.com/[a-zA-Z1-9]+\.jpg')
 
-earth_response = urllib2.urlopen('http://www.reddit.com/r/earthporn').read()
-city_response = urllib2.urlopen('http://www.reddit.com/r/cityporn').read()
-
-results1 = image_regex.findall(earth_response)[0:20]
-results2 = image_regex.findall(city_response)[0:20]
-results = set(results1 + results2)
+results = []
+for url in image_sources:
+    html = urllib2.urlopen(url).read()
+    tmp_results = image_regex.findall(html)[0:image_limit/len(image_sources)]
+    results += tmp_results
+results = set(results)
 
 dirname = os.path.dirname(sys.argv[0])
 old_images = glob.glob(dirname + '/*.jpg')
@@ -23,9 +36,10 @@ for url in results:
 
 new_image_count = len(glob.glob(dirname + '/*.jpg'))
 
-for image in old_images:
-    if new_image_count > 20:
-        os.remove(image)
-        new_image_count -= 1
-    else:
-        break
+if destructive:
+    for image in old_images:
+        if new_image_count > image_limit:
+            os.remove(image)
+            new_image_count -= 1
+        else:
+            break
